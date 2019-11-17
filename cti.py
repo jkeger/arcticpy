42e2b2ad89df1a17b2162fc4bfe928d90414ab2d
Jacob Kegerreis <jacob.kegerreis@durham.ac.uk>
2019-11-17 15:21:46 +0000

    Add main and test code files

---
+++ cti.py
@@ -0,0 +1,7 @@
+""" CTI python
+
+    Jacob Kegerreis (2019) jacob.kegerreis@durham.ac.uk
+    
+    WIP...
+"""
+
---
+++ tests.py
@@ -0,0 +1,7 @@
+""" CTI python tests
+
+    Jacob Kegerreis (2019) jacob.kegerreis@durham.ac.uk
+    
+    WIP...
+"""
+

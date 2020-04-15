cfdeb09f0736779224b7555ca5c8bb2e14511abe
Richard Massey <r.j.massey@durham.ac.uk>
2020-04-15 17:29:44 +0100

    added a test call that doesn't work

---
+++ arctic/rjm_test_slowtraps.py
@@ -0,0 +1,78 @@
+
+import numpy as np
+import pytest
+from scipy import integrate
+import matplotlib.pyplot as plt
+from copy import deepcopy
+
+import arctic as ac
+
+#Define trap parameters
+density = 0.1
+lifetime = 3
+capture_timescale=2.0
+trap = ac.Trap(density=density, lifetime=lifetime)
+trap = ac.TrapSlowCapture(density=density, lifetime=lifetime, capture_timescale=capture_timescale)
+
+
+#Define input image 
+size = 50
+background=100
+cil=1e5
+image_orig = np.zeros((size, 1))+background
+image_orig[ 3: 6, 0] = cil
+image_orig[10:16, 0] = cil
+image_orig[20:26, 0] = cil
+image_orig[30:36, 0] = cil
+image_orig[40:46, 0] = cil
+
+
+# Define instrument parameters
+ccd_volume = ac.CCDVolume(well_fill_beta=1, well_max_height=2e6, well_notch_depth=0)
+parallel_clocker = ac.Clocker()
+parallel_express = 0
+
+
+# Add CTI
+image_cti = ac.add_cti(
+    image=image_orig,
+    parallel_traps=[trap],
+    parallel_ccd_volume=ccd_volume,
+    parallel_clocker=parallel_clocker
+)
+
+
+plt.figure()
+pixels = np.arange(size)
+plt.scatter(pixels, (2+image_cti-background)[:, 0], c="k", marker=".", label="Express$=0$")
+print("total of image ",np.sum(image_cti-background))
+
+# Pure exponential for comparison
+exp_trail = np.exp(-pixels[2:] / lifetime)
+exp_trail *= image_cti[size-1, 0] / exp_trail[size-3]
+plt.plot(pixels[2:], exp_trail-background, c="k", alpha=0.3)
+plt.show(block=False)
+
+if True:
+    # Different express options
+    for express in [1,2,10]:
+        image_cti = ac.add_cti(
+            image=image_orig,
+            parallel_express=express,
+            parallel_traps=[trap],
+            parallel_ccd_volume=ccd_volume,
+            parallel_clocker=parallel_clocker
+        )
+        print((image_cti)[0:9, 0])
+        plt.plot(
+           pixels, (2+image_cti-background)[:, 0], label=r"Express$=%.1d$" % express
+        )
+
+plt.legend()
+plt.yscale("log")
+plt.xlabel("Pixel")
+plt.ylabel("Counts")
+
+
+plt.show(block=False)
+input("Press Enter to continue...")

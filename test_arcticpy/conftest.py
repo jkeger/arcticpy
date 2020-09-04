af3934338508bac7273c2f183d55f40b5b49a83a
jammy2211 <james.w.nightingale@durham.ac.uk>
2020-09-04 12:13:37 +0100

    fixing tests again :(

--- test_arcticpy/conftest.py
+++ test_arcticpy/conftest.py
@@ -1,11 +1,6 @@
 from os import path
-
-import numpy as np
 import pytest
-
-import autoarray as aa
 from autoconf import conf
-from autoarray.fit import fit
 
 directory = path.dirname(path.realpath(__file__))
 
